3b336892873cb0c905a156e3db10fa9b6d1c8d9d
Igor Franco <83724295+GiroFC@users.noreply.github.com>
2022-03-29 22:57:26 -0300

    Update TP02Q01Class.java

--- tps/entrada e saida/tp02/TP02Q01 - Classe Filme em Java/TP02Q01Class.java
+++ tps/entrada e saida/tp02/TP02Q01 - Classe Filme em Java/TP02Q01Class.java
@@ -193,7 +193,6 @@ class Filme{
         }
         this.idiomaOriginal = removeTags(linha).trim();
 
-<<<<<<< HEAD
         while(!linha.contains("Situação")){
             linha = br.readLine();
         }
@@ -219,17 +218,6 @@ class Filme{
             this.palavrasChave[i] = aux[i];
         }
 
-=======
-        while(!br.readLine().contains("Orçamento"));
-        this.orcamento = Float.parseFloat(removeTags(br.readLine()));
-
-        int i = 0;
-        while(!br.readLine().contains("Keyword")){
-            this.palavrasChave[i] = removeTags(br.readLine());
-            i++;
-        }
-        
->>>>>>> 73f7ab78da40d85cb8b68b1cb528e797851533b2
 
         br.close();
     }
