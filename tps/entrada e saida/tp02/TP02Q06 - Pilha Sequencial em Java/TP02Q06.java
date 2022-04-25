ddb4886a1c79d95ffb2c23f1f08cedf06b181237
Igor Franco <83724295+GiroFC@users.noreply.github.com>
2022-04-24 21:53:24 -0300

    tp02

---
+++ tps/entrada e saida/tp02/TP02Q06 - Pilha Sequencial em Java/TP02Q06.java
@@ -0,0 +1,642 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.*;
+
+class Filme{
+
+    //atributos privados
+    private String nome;
+    private String tituloOriginal;
+    private Date dataDeLancamento;
+    private int duracao;
+    private String genero;
+    private String idiomaOriginal;
+    private String situacao;
+    private float orcamento;
+    private String palavrasChave[];
+
+    
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    //primeiro construtor
+    Filme(){
+        nome = "";
+        tituloOriginal = "";
+        dataDeLancamento = null;
+        duracao = 0;
+        genero = "";
+        idiomaOriginal = "";
+        situacao = "";
+        orcamento = 0;
+        palavrasChave = null;
+    }
+
+    //Segundo construtor, agora recebendo argumentos
+    Filme(String nome, String tituloOriginal, Date dataDeLancamento, int duracao, String genero, String idiomaOriginal, String situacao, float orcamento, String palavrasChave[]){
+        this.nome = nome;
+        this.tituloOriginal = tituloOriginal;
+        this.dataDeLancamento = dataDeLancamento;
+        this.duracao = duracao;
+        this.genero = genero;
+        this.idiomaOriginal = idiomaOriginal;
+        this.situacao = situacao;
+        this.orcamento = orcamento;
+        this.palavrasChave = palavrasChave;
+    }
+
+    //set de stributos
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTituloOriginal(String tituloOriginal) {
+        this.tituloOriginal = tituloOriginal;
+    }
+
+    public void setDataDeLancamento(Date dataDeLancamento) {
+        this.dataDeLancamento = dataDeLancamento;
+    }
+    
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setIdiomaOriginal(String idiomaOriginal) {
+        this.idiomaOriginal = idiomaOriginal;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public void setOrcamento(float orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public void setPalavrasChave(String palavrasChave[]) {
+        this.palavrasChave = palavrasChave;
+    }
+
+    //get de atributos
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTituloOriginal() {
+        return tituloOriginal;
+    }
+
+    public Date getDataDeLancamento() {
+        return dataDeLancamento;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getIdiomaOriginal() {
+        return idiomaOriginal;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public float getOrcamento() {
+        return orcamento;
+    }
+
+    public String[] getPalavrasChave() {
+        return palavrasChave;
+    }
+
+    //removendo tags html
+    public String removeTags(String line){
+        String nova="";
+        int i = 0;
+        while(i<line.length()){
+            if(line.charAt(i)== '<'){
+                i++;
+                while(line.charAt(i) != '>')i++;
+            }else{
+                nova += line.charAt(i);
+            }
+            i++;
+        }
+        nova = nova.replace("&nbsp;", " ");
+        nova = nova.replace("Título original", " ");
+        nova = nova.replace("Idioma original", " ");
+        nova = nova.replace("Situação", " ");
+        nova = nova.replace("(BR)", " ");
+        nova = nova.replace("Orçamento", " ");
+        nova = nova.replace("$", " ");
+        nova = nova.replace(",", "");
+
+        return nova;
+    }
+
+
+    //transformando o array de duração em um inteiro de minutos
+    public int duracaoToInt(String s){
+        String aux[];
+        String minuto = "";
+        int minutosTotais;
+
+        if(s.contains("h") && s.contains("m")){
+            aux = s.split("h");
+            minuto = aux[1].substring(0, aux[1].length() - 1).trim();
+            minutosTotais = (Integer.parseInt(aux[0].trim()) * 60) + (Integer.parseInt(minuto));
+        } else if(s.contains("m") == false){
+            s = s.replace("h", " ");
+            minutosTotais = ((Integer.parseInt(s.trim())) * 60);
+        } else{
+            minuto += s.trim();
+            minuto = minuto.replace("m", " ");
+            
+            minutosTotais = Integer.parseInt(minuto.trim());
+        }
+
+        // return minutosTotais;
+        return minutosTotais;
+    }
+
+
+    //lendo o array ate achar um parenteses
+    public String ateParenteses(String line){
+        String nova = "";
+        for(int i = 0; i < line.length(); i++){
+            if(line.charAt(i) != '('){
+                nova+=line.charAt(i);
+            } else { 
+                return nova;
+            }
+        }
+        return nova;
+        
+    }
+    //Leitor
+    public void ler(String nomeArquivo) throws Exception{
+        InputStreamReader fr = new FileReader(nomeArquivo);
+        BufferedReader br = new  BufferedReader(fr);
+        String linha = br.readLine();
+
+
+        //Lendo o nome
+        while(!(linha=br.readLine()).contains("<title>"));
+        this.nome = ateParenteses(removeTags(linha).trim());
+
+
+        //Lendo a data de lançamento
+        
+        while(!(linha=br.readLine()).contains("span class=\"release\""));
+        linha = br.readLine();
+        this.dataDeLancamento = sdf.parse(ateParenteses(removeTags(linha).trim()));
+
+
+        //Lendo os generos 
+        while(!(linha=br.readLine()).contains("genres"));
+        br.readLine();
+        linha = br.readLine();
+        linha = removeTags(linha.trim());
+        String auxi[];
+        auxi = linha.split(" ");
+        this.genero = palavraToStringNoSpace(auxi);
+
+
+        //Lendo a duração
+        while(!(linha=br.readLine()).contains("runtime"));
+        br.readLine();
+        linha = br.readLine();
+        this.duracao = duracaoToInt(removeTags(linha).trim());
+
+
+        //Lendo o Titulo Original
+
+        while(!(linha=br.readLine()).contains("<section class=\"facts left_column\">"));
+        while(!(linha=br.readLine()).contains("<strong><bdi>Situação</bdi></strong>")){
+            if((linha=br.readLine()).contains("Título original")){
+                this.tituloOriginal = removeTags(linha).trim();
+            }
+        }
+
+        if(tituloOriginal == ""){
+            this.tituloOriginal = nome;
+        }
+        
+        
+        // while(!(linha=br.readLine()).contains("Título original"));
+        // this.tituloOriginal = removeTags(linha).trim();
+        
+
+        //lendo a situação
+        //while(!(linha=br.readLine()).contains("Situação</bdi>"));
+        this.situacao = removeTags(linha).trim();
+
+        //Lendo o idioma
+        while(!(linha=br.readLine()).contains("Idioma original"));
+        this.idiomaOriginal = removeTags(linha).trim();
+
+        
+        //Lendo o orçamento
+        while(!(linha=br.readLine()).contains("Orçamento"));
+        if(linha.contains("<p><strong><bdi>Orçamento</bdi></strong> -</p>")){
+            linha = removeTags(linha).trim();
+            this.orcamento = 0;
+        }else{
+        this.orcamento = Float.parseFloat(removeTags(linha).trim());
+        }
+
+        //Lendo as palavras-chave
+        //e verificando se nao tem nenhuma
+        while(!(linha=br.readLine()).contains("Palavras-chave"));
+        linha = "";
+        br.readLine();
+        linha=br.readLine();
+        if(linha.contains("Nenhuma palavra-chave foi adicionada.")){
+            this.palavrasChave = removeTags(linha).trim().split("--");
+        }else{
+            while(!(linha+=br.readLine().trim()+"-").contains("</ul>"));
+            this.palavrasChave = removeTags(linha).trim().split("--");
+        }
+        
+
+        br.close();
+    }
+
+    //convertendo as palavras chave para uma unica string
+    public String palavraToString(String s[]){
+        String aux = "";
+        int i = 0;
+        if(s[0].contains("Nenhuma palavra-chave foi adicionada.")){
+            aux = "";
+        }else{
+            for(; i < s.length - 2; i++){
+                aux+=s[i];
+                aux+=  ", ";
+            }
+            aux+=s[i];
+        }
+        // for(; i < s.length - 2; i++){
+        //     aux+=s[i];
+        //     aux+=  ", ";
+        // }
+        // aux+=s[i];
+        return aux.replace("-", "");
+    }
+
+    public String palavraToStringNoSpace(String s[]){
+        String aux = "";
+        int i = 0;
+        if(s[0].contains("Nenhuma palavra-chave foi adicionada.")){
+            aux = "";
+        }else{
+            for(; i < s.length - 1; i++){
+                aux+=s[i];
+                aux+=  ",";
+            }
+            aux+=s[i];
+        }
+        // for(; i < s.length - 2; i++){
+        //     aux+=s[i];
+        //     aux+=  ", ";
+        // }
+        // aux+=s[i];
+        return aux.replace("-", "");
+    }
+
+    //clone
+    public Filme clone(){
+        Filme resp = new Filme();
+        resp.nome = this.nome;
+        resp.tituloOriginal = this.tituloOriginal;
+        resp.dataDeLancamento = this.dataDeLancamento;
+        resp.duracao = this.duracao;
+        resp.genero = this.genero;
+        resp.idiomaOriginal= this.idiomaOriginal;
+        resp.situacao = this.situacao;
+        resp.orcamento = this.orcamento;
+        resp.palavrasChave = this.palavrasChave;
+        return resp;
+    }
+
+    //imprimir
+    public void Imprimir(){
+        MyIO.println(this.nome + this.tituloOriginal + " " + sdf.format(this.dataDeLancamento) + " " + this.duracao + " " + this.genero + " " + this.idiomaOriginal + " " + this.situacao + " " + this.orcamento  + " [" + palavraToString(this.palavrasChave) + "]");
+    }
+    
+
+    
+}
+
+/**
+ * Lista estatica
+ * @author Max do Val Machado
+ * @version 2 01/2015
+ */
+class Pilha {
+    private Filme[] array;
+    private int n;
+
+    public int getN(){
+        return this.n;
+    }
+ 
+    /**
+     * Construtor da classe.
+     */
+    public Pilha () {
+       this(6);
+    }
+ 
+ 
+    /**
+     * Construtor da classe.
+     * @param tamanho Tamanho da lista.
+     */
+    public Pilha (int tamanho){
+       array = new Filme[tamanho];
+       n = 0;
+    }
+ 
+ 
+    /**
+     * Insere um elemento na primeira posicao da lista e move os demais
+     * elementos para o fim da lista.
+     * @param x int elemento a ser inserido.
+     * @throws Exception Se a lista estiver cheia.
+     */
+    // public void inserirInicio(Filme x) throws Exception {
+ 
+    //    //validar insercao
+    //    if(n >= array.length){
+    //       throw new Exception("Erro ao inserir!");
+    //    } 
+ 
+    //    //levar elementos para o fim do array
+    //    for(int i = n; i > 0; i--){
+    //       array[i] = array[i-1];
+    //    }
+ 
+    //    array[0] = x;
+    //    n++;
+    // }
+ 
+ 
+    /**
+     * Insere um elemento na ultima posicao da lista.
+     * @param x int elemento a ser inserido.
+     * @throws Exception Se a lista estiver cheia.
+     */
+    public void inserir(Filme x) throws Exception {
+ 
+       //validar insercao
+       if(n >= array.length){
+          throw new Exception("Erro ao inserir!");
+       }
+ 
+       array[n] = x;
+       n++;
+    }
+ 
+ 
+    /**
+     * Insere um elemento em uma posicao especifica e move os demais
+     * elementos para o fim da lista.
+     * @param x int elemento a ser inserido.
+     * @param pos Posicao de insercao.
+     * @throws Exception Se a lista estiver cheia ou a posicao invalida.
+     */
+    // public void inserir(Filme x, int pos) throws Exception {
+ 
+    //    //validar insercao
+    //    if(n >= array.length || pos < 0 || pos > n){
+    //       throw new Exception("Erro ao inserir!");
+    //    }
+ 
+    //    //levar elementos para o fim do array
+    //    for(int i = n; i > pos; i--){
+    //       array[i] = array[i-1];
+    //    }
+ 
+    //    array[pos] = x;
+    //    n++;
+    // }
+ 
+ 
+    /**
+     * Remove um elemento da primeira posicao da lista e movimenta 
+     * os demais elementos para o inicio da mesma.
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se a lista estiver vazia.
+     */
+    // public Filme removerInicio() throws Exception {
+ 
+    //    //validar remocao
+    //    if (n == 0) {
+    //       throw new Exception("Erro ao remover!");
+    //    }
+ 
+    //    Filme resp = array[0];
+    //    n--;
+ 
+    //    for(int i = 0; i < n; i++){
+    //       array[i] = array[i+1];
+    //    }
+ 
+    //    return resp;
+    // }
+ 
+ 
+    /**
+     * Remove um elemento da ultima posicao da lista.
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se a lista estiver vazia.
+     */
+    public Filme remover() throws Exception {
+ 
+       //validar remocao
+       if (n == 0) {
+          throw new Exception("Erro ao remover!");
+       }
+ 
+       return array[--n];
+    }
+ 
+ 
+    /**
+     * Remove um elemento de uma posicao especifica da lista e 
+     * movimenta os demais elementos para o inicio da mesma.
+     * @param pos Posicao de remocao.
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se a lista estiver vazia ou a posicao for invalida.
+     */
+    // public Filme remover(int pos) throws Exception {
+ 
+    //    //validar remocao
+    //    if (n == 0 || pos < 0 || pos >= n) {
+    //       throw new Exception("Erro ao remover!");
+    //    }
+ 
+    //    Filme resp = array[pos];
+    //    n--;
+ 
+    //    for(int i = pos; i < n; i++){
+    //       array[i] = array[i+1];
+    //    }
+ 
+    //    return resp;
+    // }
+ 
+ 
+    /**
+     * Mostra os elementos da lista separados por espacos.
+     */
+    public void mostrar (){
+       System.out.print("[ ");
+       for(int i = 0; i < n; i++){
+          System.out.print(array[i] + " ");
+       }
+       System.out.println("]");
+    }
+ 
+ 
+    /**
+     * Procura um elemento e retorna se ele existe.
+     * @param x int elemento a ser pesquisado.
+     * @return <code>true</code> se o array existir,
+     * <code>false</code> em caso contrario.
+     */
+
+    public int comparacoes = 0;
+    public boolean pesquisaSequencial(String nomeP){
+        boolean resp = false;
+        for(int i = 0; i < n; i++){
+            // System.out.println(entrada[i].getNome());
+            // System.out.println(nomeP);
+            if(((array[i].getNome()).trim()).equals(nomeP) == true){
+                resp = true;
+                i = n;
+            }else{
+                resp = false;
+            }
+        }
+        return resp;
+    }
+
+    public boolean pesquisaBinaria(String nomeP){
+        boolean resp = false;
+        int dir = n, esq = 0, meio;
+        
+        while(esq < dir){
+            meio = (esq + dir) / 2;
+            
+            // System.out.println(array[meio].getNome());
+            // System.out.println(nomeP);
+            if(nomeP.equals((array[meio].getNome()).trim()) == true){
+                resp = true;
+                esq = dir;
+            }else if(nomeP.compareTo((array[meio].getNome()).trim()) > 0){ 
+                esq = meio + 1;
+            } else{
+                dir = meio - 1;
+            }
+        }
+        return resp;
+    }
+
+    public void imprimirA(){
+        for(int i = 0; i < n; i++){
+            System.out.print("[");
+            System.out.print(i);
+            System.out.print("] ");
+            array[i].Imprimir();
+            
+        }
+    }
+ }
+ 
+
+
+
+public class TP02Q06{
+
+    public static int contador = 0; 
+
+    public static long now(){
+        return new Date().getTime();
+    }
+
+    public static boolean isFim(String s) {
+        return(s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    public static void main(String[] args) throws Exception{
+        String[] input = new String[1000];
+        int numInput = 0;
+        MyIO.setCharset("UTF-8");
+
+        do{
+            input[numInput] = MyIO.readLine();
+        }while(isFim(input[numInput++]) == false);
+        numInput--;//Desconsiderar a palavra FIM
+            
+        Pilha pilha = new Pilha(100);
+    
+        //lendo o arquivo dos filmes
+        for(int i = 0; i < numInput;i++){
+            Filme aux = new Filme();
+            aux.ler("/tmp/filmes/"+input[i]);
+            pilha.inserir(aux);
+            // filmes[i].Imprimir();
+        }
+
+        //lendo o nome para a verificação
+        int n = MyIO.readInt();
+        String comando;
+        String comandoP = "";
+        String pos = "";
+        String removidos[] = new String[50];
+
+        for(int i = 0; i < n; i++){
+            comando = MyIO.readLine();
+            comandoP += comando.charAt(0);
+            // System.out.println(comandoP);
+            // separador = comando.split(" ");
+            comando = comando.trim();
+            comando = comando.substring(1);
+            comando = comando.trim();
+            // System.out.println(comando);
+            
+            Filme aux = new Filme();
+            
+            if(comandoP.equals("I") == true){
+                aux.ler("/tmp/filmes/"+comando);
+                pilha.inserir(aux);
+            }else if(comandoP.equals("R") == true){
+                System.out.print("(R) ");
+                System.out.println((pilha.remover()).getNome());
+            } 
+
+            pos = "";
+            comandoP = "";
+        } 
+
+        pilha.imprimirA();
+    }
+}
+
+
